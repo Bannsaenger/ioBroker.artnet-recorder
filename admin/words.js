35087bcc638a384af991d3201686883a4a7f0517
Bannsaenger <bannsaenger@gmx.de>
2021-06-11 12:53:53 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'artnet-recorder adapter settings': {
+        'en': 'Adapter settings for artnet-recorder',
+        'de': 'Adaptereinstellungen für artnet-recorder',
+        'ru': 'Настройки адаптера для artnet-recorder',
+        'pt': 'Configurações do adaptador para artnet-recorder',
+        'nl': 'Adapterinstellingen voor artnet-recorder',
+        'fr': "Paramètres d'adaptateur pour artnet-recorder",
+        'it': "Impostazioni dell'adattatore per artnet-recorder",
+        'es': 'Ajustes del adaptador para artnet-recorder',
+        'pl': 'Ustawienia adaptera dla artnet-recorder',
+        'zh-cn': 'artnet-recorder的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
