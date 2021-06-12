aff6f70d459329f70d4749b80aa032035060f37e
Bannsaenger <bannsaenger@gmx.de>
2021-06-12 18:13:34 +0200

    in progress

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ const utils = require('@iobroker/adapter-core');
 // Load your modules here, e.g.:
 const fs = require('fs');
 const udp = require('dgram');
+const lineByLine = require('n-readlines');
+//const path = require('path');
 
 class ArtnetRecorder extends utils.Adapter {
 
@@ -41,7 +43,7 @@ class ArtnetRecorder extends utils.Adapter {
         this.objectsTemplate = JSON.parse(fs.readFileSync(__dirname + '/lib/objects_templates.json', 'utf8'));
 
         // set some global variables
-        this.recordRuning = false;                                                              // true if recording process is running
+        this.recordRunning = false;                                                             // true if recording process is running
         this.recordStartTime = 0;                                                               // takes the time when recording has started
         this.playbackRunning = false;                                                           // true if a playback is in progress
         // prepare buffers for header comparison
@@ -49,6 +51,9 @@ class ArtnetRecorder extends utils.Adapter {
         this.artNetOpcode  = Buffer.from([0x00, 0x50]);                                         // 0x5000 little Endian
         this.artNetVersion = Buffer.from([0x00, 0x0E]);                                         // Protocol Version 0 14
         this.artNetBuffer = undefined;                                                          // filled later
+        this.workingDir = '';                                                                   // filled with the working directory
+        this.canRecord = true;                                                                  // false if working directory is not writable
+        this.recFile = undefined;                                                               // the file which is opened for recording
 
         // creating a udp server
         this.server = udp.createSocket('udp4');
@@ -86,7 +91,7 @@ class ArtnetRecorder extends utils.Adapter {
                 await self.setObjectNotExistsAsync(element._id, element);
             }
             for (const element of self.objectsTemplate.control) {
-                await self.setObjectNotExistsAsync(element._id, element);
+                await self.setObjectNotExistsAsync(`control.${element._id}`, element);
             }
 
             self.artNetBuffer = Buffer.alloc(Number(self.config.maxDmxAddress));    // create a internal buffer
@@ -98,6 +103,35 @@ class ArtnetRecorder extends utils.Adapter {
             // try to open open configured server port
             self.log.info('Bind UDP socket to: "' + self.config.bind + ':' + self.config.port + '"');
             self.server.bind(self.config.port, self.config.bind);
+
+            // check if the given workingDirectory exists.
+            // @ts-ignore
+            const tempObj = await self.getStateAsync('control.workingDir');
+            // @ts-ignore
+            self.workingDir = (tempObj && tempObj.val) ? tempObj.val.toString() : '.';
+
+            if (fs.existsSync(self.workingDir)) {
+                const locWritable = await fs.accessSync(self.workingDir, fs.constants.R_OK | fs.constants.W_OK);
+                if (locWritable == undefined) {
+                    self.log.info(`Art-Net Recorder working directory: ${self.workingDir} exists and is writable.`);
+                } else {
+                    self.log.info(`Art-Net Recorder working directory: ${self.workingDir} exists but is not writable. Recording not possible.`);
+                    self.canRecord = false;
+                }
+            } else {
+                self.log.error(`Art-Net Recorder working directory: ${self.workingDir} did not exist. Terminating`);
+                self.terminate();
+            }
+
+            const obj1 = new lineByLine(__dirname + '/lib/testbuckets.rec', {'readChunk': 15000, 'newLineCharacter': '\n'});
+            const line1 = JSON.parse(obj1.next());
+            const line2 = JSON.parse(obj1.next());
+            const line3 = JSON.parse(obj1.next());
+            //obj1.close();
+
+            self.log.silly(`${line1}${line2}${line3}`);
+            // reset the mode on startup
+            self.setState('control.mode', 0, true);
 
             // Set the connection indicator after startup
             // self.setState('info.connection', true, true);
@@ -178,34 +212,28 @@ class ArtnetRecorder extends utils.Adapter {
             const msgUniverse = msg[15] * 256 + msg[14];
             const msgLength   = msg[16] * 256 + msg[17];
             self.log.debug(`Art-Net Recorder received packet with sequence: ${msgSequence}, physical: ${msgPhysical}, universe: ${msgUniverse}, length: ${msgLength}`);
-            //const msg_hex = msg.toString('hex').toUpperCase();
-            //self.log.silly(`${msg_hex} ${info}`);
-            if (!self.playbackRunning && !self.recordRuning) {          // no playback or recording in progress. Only save the buffer and return
+            if (!self.recordRunning) {          // no recording in progress. Only save the buffer and return
                 self.artNetBuffer = msg.slice(18, msg.length);
                 return;
             }
+            // from here there is playback or record running. Check whether it is dirty against the buffer
+            const dmxVals = msg.slice(18, msg.length);
+            const dmxValsChanged = [];
+            let isDirty = false;
+
+            for (let actBucket = 0; actBucket < dmxVals.length; actBucket++) {
+                if (dmxVals[actBucket] != self.artNetBuffer[actBucket]) {
+                    isDirty = true;
+                    dmxValsChanged.push({'channel': actBucket + 1, 'value': dmxVals[actBucket]});
+                }
+            }
+            if (isDirty) {
+                this.log.silly(JSON.stringify(dmxValsChanged));
+            }
+            self.artNetBuffer = dmxVals;
 
         } catch (err) {
             self.errorHandler(err, 'onServerMessage');
-        }
-    }
-
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     * @param {() => void} callback
-     */
-    onUnload(callback) {
-        try {
-            const self = this;
-            // Reset the connection indicator
-            self.setState('info.connection', false, true);
-
-            // Here you must clear all timeouts or intervals that may still be active
-
-            // close the server port
-            this.server.close(callback);
-        } catch (e) {
-            callback();
         }
     }
 
@@ -215,12 +243,82 @@ class ArtnetRecorder extends utils.Adapter {
      * @param {ioBroker.State | null | undefined} state
      */
     onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
+        const self = this;
+        try {
+            if (state) {
+                // The state was changed
+                // self.log.debug(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+                if (!state.ack) {                   // only react on not acknowledged state changes
+                    if (state.lc === state.ts) {    // last changed and last updated equal then the value has changed
+                        self.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+                        if (/mode/.test(id)) {
+                            self.log.info(`Art-Net Recorder mode ${id} changed to ${state.val}`);
+                            self.playbackRunning = false;
+                            self.recordRunning = false;
+                            switch (state.val) {
+                                case 1:             // Record
+                                    if (self.canRecord) {
+                                        self.recordRunning = true;
+                                        self.createRecordFile();
+                                        self.log.info(`Art-Net Recorder switched to Record mode`);
+                                    } else {
+                                        self.log.warn(`Art-Net Recorder can not record. No writable directory`);
+                                    }
+                                    break;
+
+                                case 2:             // Playback
+                                    self.log.info(`Art-Net Recorder switched to Playback mode`);
+                                    self.playbackRunning = true;
+                                    if (self.recFile) {
+                                        self.log.debug(`Art-Net Recorder closed recording file`);
+                                        self.recFile.close();
+                                    }
+                                    break;
+
+                                default:
+                                    self.log.info(`Art-Net Recorder switched off`);
+                            }
+                        }
+                    }
+                    else {
+                        self.log.debug(`state ${id} only updated not changed: ${state.val} (ack = ${state.ack})`);
+                    }
+                }
+            } else {
+                // The state was deleted
+                self.log.info(`state ${id} deleted`);
+            }
+        } catch (err) {
+            self.errorHandler(err, 'onStateChange');
+        }
+    }
+
+    /**
+     * Called for creating a new file for recording
+	 */
+    createRecordFile() {
+        const self = this;
+        try {
+            const locDateObj = new Date();
+            // current date
+            // current month
+            const locMonth = ('0' + (locDateObj.getMonth() + 1)).slice(-2);
+            // current day
+            const locDay = ('0' + (locDateObj.getDay() + 1)).slice(-2);
+            // current year
+            const locYear = locDateObj.getFullYear();
+            // current hours
+            const locHours = locDateObj.getHours();
+            // current minutes
+            const locMinutes = locDateObj.getMinutes();
+            // current seconds
+            const locSeconds = locDateObj.getSeconds();
+            const locFileName = `${locYear}${locMonth}${locDay}_${locHours}${locMinutes}${locSeconds}_Art-Net_Record.rec`;
+            // try to open the file
+            self.recFile = fs.openSync(`${self.workingDir}/${locFileName}`, 'w');
+            self.log.error(`Art-Net Recorder opened file:${locFileName} for recording`);
+        } catch (err) {
+            self.errorHandler(err, 'createRecordFile');
         }
     }
 
@@ -251,6 +349,30 @@ class ArtnetRecorder extends utils.Adapter {
     //     }
     // }
 
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            const self = this;
+            // Reset the connection indicator
+            self.setState('info.connection', false, true);
+
+            // Here you must clear all timeouts or intervals that may still be active
+
+            // close the recording file
+            if (self.recFile) {
+                self.log.warn(`Art-Net Recorder closed recording file on unload`);
+                self.recFile.close();
+            }
+
+            // close the server port
+            this.server.close(callback);
+        } catch (e) {
+            callback();
+        }
+    }
 }
 
 // @ts-ignore parent is a valid property on module
